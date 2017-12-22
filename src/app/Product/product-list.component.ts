f5d8ce28bae9ab145c7c965289e58c157775d209
Uzair <ozairWani@github.com>
2017-12-22 21:18:58 +0800

    filtering

--- src/app/Product/product-list.component.ts
+++ src/app/Product/product-list.component.ts
@@ -9,10 +9,22 @@ import { IProduct } from "./product";
 
 export class ProductListComponent{
   pageTitle:string="Product List"
-  imageWidth:number =100;
-  imageMargin:number =2;
+  imageWidth:number = 50;
+  imageMargin:number = 10;
   showImage :boolean = false; 
-  filterProduct :string = 'Leaf Rake'
+
+  _listFilter: string;
+
+  get listFilter():string{
+    return this._listFilter;
+  }
+
+  set listFilter(value:string){
+    this._listFilter = value;
+    this.filteredProduct = this.listFilter?this.performFilter(this.listFilter):this.products;
+  }
+
+  filteredProduct :IProduct[];
 
   products: IProduct[] =[
     {
@@ -34,8 +46,54 @@ export class ProductListComponent{
         "price": 32.99,
         "starRating": 4.2,
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+  },
+  {
+      "productId": 8,
+      "productName": "Saw",
+      "productCode": "TBX-0022",
+      "releaseDate": "May 15, 2016",
+      "description": "15-inch steel blade hand saw",
+      "price": 11.55,
+      "starRating": 3.7,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+  },
+  {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2015",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+  }
   ]
+ 
+  /**
+   *
+   */
+  constructor() {
+    this.filteredProduct = this.products;
+   
+  }
+
+  performFilter(filterBy:string):IProduct[]{
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter((product:IProduct)=>
+           product.productName.toLocaleLowerCase().indexOf(filterBy)!== -1 )
+    
+  }
+
   toggleImage():void{
     this.showImage = !this.showImage
   }
